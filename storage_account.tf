03cd690a334854d41a7f2792f2fd2811bd418f49
Luke <103580092+L-u-k-e-GIT@users.noreply.github.com>
2022-09-15 14:07:04 +0200

    first version

---
+++ storage_account.tf
@@ -0,0 +1,85 @@
+##  Storage account for project ##########
+resource "azurerm_storage_account" "st_data" {
+   
+    name                        = var.MD_ST_NAME 
+    resource_group_name         = var.MD_RG_NAME
+    location                    = var.MD_LOCATION
+    account_tier                = var.MD_STORAGE_ACCOUNT_TIER
+    account_replication_type    = var.MD_STORAGE_ACCOUNT_REPLICATION_TYPE
+    access_tier                 = var.MD_STORAGE_ACCOUNT_ACCESS_TIER 
+    is_hns_enabled              = var.MD_STORAGE_ACCOUNT_HNS_ENABLED
+    min_tls_version             = var.MD_STORAGE_ACCOUNT_MIN_TLS
+    allow_nested_items_to_be_public  = false
+    tags = var.MD_ALL_TAGS
+   network_rules {
+             bypass = ["AzureServices"] #to be one of [AzureServices Logging Metrics None]
+             default_action   = "Deny"
+             ip_rules                   = var.MD_STORAGE_ACCOUNT_WHITE_IP
+        }
+      
+    share_properties  {
+      retention_policy { days = var.MD_STORAGE_ACCOUNT_SHARE_RETENTION_POLICY}
+      smb {
+      versions = var.MD_STORAGE_ACCOUNT_SHARE_SMB_VERSIONS
+      authentication_types = var.MD_STORAGE_ACCOUNT_SHARE_SMB_AUTH
+      kerberos_ticket_encryption_type = var.MD_STORAGE_ACCOUNT_SHARE_SMB_KERBEROS_ENCRYPTION_TYPE
+      channel_encryption_type = []
+     }
+
+    }
+
+    lifecycle {
+    ignore_changes = [
+     tags
+    ]
+  }
+}
+
+
+
+
+#PRIVATE ENDPOINT FOR THE STORAGE ACCOUNT IN THE SAME SUBNET OF THE PROJECT
+
+
+
+
+resource "azurerm_private_endpoint" "pe_st" {
+  
+  name                = var.MD_PE_NAME
+  location            = var.MD_LOCATION
+  resource_group_name = var.MD_RG_NAME
+  subnet_id           = var.MD_SUBNET_ID
+  tags = var.MD_ALL_TAGS
+  private_dns_zone_group {
+    name                 = var.MD_DNS_privatelink
+    private_dns_zone_ids = ["${var.MD_DNS_privatelink_prefix}${var.MD_DNS_privatelink}"]
+  }
+
+  private_service_connection {
+    name                              = var.MD_PESC_NAME
+    private_connection_resource_id = azurerm_storage_account.st_data.id
+    is_manual_connection              = false
+    subresource_names = ["file"]
+  }
+  lifecycle {
+    ignore_changes = [
+     tags
+    ]
+  }
+
+  
+  depends_on = [
+    azurerm_storage_account.st_data
+  ]
+
+}
+
+
+output "storage_account_name" {
+  value = azurerm_storage_account.st_data.name
+}
+
+output "storage_account_id" {
+  
+  value = azurerm_storage_account.st_data.id
+}
---
+++ variables.tf
@@ -0,0 +1,177 @@
+variable "MD_STORAGE_ACCOUNT_TIER" {
+    description = "Storage Account Tier"
+    type        = string
+    default     = ""
+  }
+   
+variable "MD_STORAGE_ACCOUNT_REPLICATION_TYPE" {
+    description = "Storage account replication type"
+    type        = string
+    default     = ""
+  }
+
+variable "MD_STORAGE_ACCOUNT_ACCESS_TIER" {
+    description = "Storage Account Tier"
+    type        = string
+    default     = ""
+  }
+
+variable "MD_STORAGE_ACCOUNT_HNS_ENABLED" {
+    description = "enable hns on storage account"
+    type        = bool
+    default     = false
+  }
+
+ variable "MD_STORAGE_ACCOUNT_MIN_TLS" {
+    description = "Minimum version of TLS"
+    type        = string
+    default     = ""
+  }
+
+ variable "MD_STORAGE_ACCOUNT_WHITE_IP" {
+    description = "Public IP that deploy stotage account"
+    type        = list
+    default     = []
+  }
+
+ variable "MD_STORAGE_ACCOUNT_SHARE_RETENTION_POLICY" {
+    description = "Public IP that deploy stotage account"
+    type        = string
+    default     = ""
+  }
+
+
+ variable "MD_STORAGE_ACCOUNT_SHARE_SMB_VERSIONS" {
+    description = "Supported SMB version"
+    type        = list
+    default     = []
+  }
+
+ variable "MD_STORAGE_ACCOUNT_SHARE_SMB_AUTH" {
+    description = "Supperted SMB authentication type"
+    type        = list
+    default     = []
+  }
+
+  
+ variable "MD_STORAGE_ACCOUNT_SHARE_SMB_KERBEROS_ENCRYPTION_TYPE" {
+    description = "Supperted kerberos encryption type"
+    type        = list
+    default     = []
+  }
+   
+ 
+
+
+
+variable "MD_DIAG_STORAGE_ACCOUNT" { 
+ description = "Nome dello storage account diagnostics"
+ type = string
+ default = ""
+ 
+}
+
+
+variable "MD_RG_NAME" { 
+ description = "Nome del resource group"
+ type = string
+ default = ""
+}
+
+variable "MD_ST_NAME" { 
+ description = "Nome dello storage account "
+ type = string
+ default = ""
+}
+
+variable "MD_PE_NAME" { 
+ description = "Nome del private endpoint "
+ type = string
+ default = ""
+}
+
+
+variable "MD_PESC_NAME" { 
+ description = "Nome del private endpoint service connection "
+ type = string
+ default = ""
+}
+
+
+/*
+variable "MD_PROJECT_NAME" { 
+ description = "Nome dello storage accound diagnostics"
+ type = string
+ default = ""
+}*/
+
+variable "MD_DNS_privatelink_prefix" { 
+ description = "DNS zone prefix"
+ type = string
+ default = ""
+}
+
+
+variable "MD_DNS_privatelink" { 
+ description = "DNS zone name"
+ type = string
+ default = ""
+}
+
+/*
+variable "MD_SUBSCRIPTION_PREFIX" { 
+ description = "Nome dello storage accound diagnostics"
+ type = string
+ default = ""
+}*/
+
+variable "MD_LOCATION" { 
+ description = "Nome dello storage accound diagnostics"
+ type = string
+ default = ""
+}
+
+/*
+variable "MD_ST_PREFIX" { 
+ description = "Nome dello storage accound diagnostics"
+ type = string
+ default = ""
+}
+*/
+
+ variable "MD_PE_PREFIX" {
+  type        = string
+  description = "Prefisso pre private endpoint"
+  default     = ""
+}
+
+ variable "MD_REGION_PREFIX" {
+  type        = string
+  description = "Prefisso pre private endpoint"
+  default     = ""
+}
+
+
+variable "MD_SUBNET_ID" {
+  type        = string
+  description = "ID della subnet"
+  default     = ""
+}
+
+
+variable "MD_BCK_VAULT_PREFIX" {
+  type        = string
+  description = "ID della subnet"
+  default     = ""
+}
+
+
+variable "MD_BCK_POLICY_ST_PREFIX" { 
+ description = "Prefisso Backup Storage Account Policy"
+ type = string
+ default = ""
+}
+
+variable "MD_ALL_TAGS" { }
+
+
